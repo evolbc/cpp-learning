a3e1a435e1b2c6c68675c44766dec96fcdb21fc2
Evol <evol.bellecoeur@gmail.com>
2013-07-18 14:44:17 -0800

    ch0 work

---
+++ ch0/exercise3.cpp
@@ -0,0 +1,11 @@
+// Chapter 0, Exercise 3
+// Experiment with the string literal "\t"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "\tOne Tab" << std::endl;
+    std::cout << "\t\tTwo Tabs" << std::endl;
+    return 0;
+}
---
+++ ch0/exercise4.cpp
@@ -0,0 +1,16 @@
+// Chapter 0, Exercise 4
+// Write the "Hello, world!" code.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "// a small C++ program" << std::endl;
+    std::cout << "#include <iostream>\n" << std::endl;
+    std::cout << "int main()" << std::endl;
+    std::cout << "{" << std::endl;
+    std::cout << "\tstd::cout << \"Hello, world!\" << std::endl;" << std::endl;
+    std::cout << "\treturn 0;" << std::endl;
+    std::cout << "}" << std::endl;
+    return 0;
+}
